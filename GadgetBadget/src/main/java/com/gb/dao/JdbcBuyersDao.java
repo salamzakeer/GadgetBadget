264a68b92cb0880c87312ab2dbe3ecfe6570ca9e
Noha_Menani <nohamenani3107@gmail.com>
2021-04-24 11:34:16 +0530

    addBuyers added

--- GadgetBadget/src/main/java/com/gb/dao/JdbcBuyersDao.java
+++ GadgetBadget/src/main/java/com/gb/dao/JdbcBuyersDao.java
@@ -15,6 +15,36 @@ public class JdbcBuyersDao implements BuyersDao {
 
 //add buyer
 
+	@Override
+	public Buyer addBuyer(Buyer buyer) throws DaoException {
+		String sql = "insert into buyers(fname,lname,pnumber,email,password) values (?,?,?,?,?)";
+		
+		try(
+				Connection conn= DbUtil.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		){
+			stmt.setString(1, buyer.getFname());
+			stmt.setString(2, buyer.getLname());
+			stmt.setString(3, buyer.getPnumber());
+			stmt.setString(4, buyer.getEmail());
+			stmt.setString(5, buyer.getPassword());
+			
+			stmt.executeUpdate();
+			ResultSet keys = stmt.getGeneratedKeys();
+			keys.next();
+			keys.getInt(1);
+			buyer.setbId(keys.getInt(1));
+			
+			return buyer;
+		}
+		catch (Exception ex) {
+			throw new DaoException(ex);
+		}
+		
+	
+	}
+	
+
 //findviewbyid
 
 	private Buyer toBuyer(ResultSet rs) throws SQLException {
@@ -30,7 +60,7 @@ public class JdbcBuyersDao implements BuyersDao {
 
 	//updatebuyer
 
-//deletebuy
+	//deletebuyer
 
 	@Override
 	public List<Buyer> findAll() throws DaoException {
